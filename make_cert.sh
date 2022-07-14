8df70536fd98db3ed7eedaf3e3452783e6443de7
DouglasWWolf <douglaswolf@yahoo.com>
2022-07-14 00:26:27 -0700

    No change history kept

--- make_cert.sh
+++ make_cert.sh
@@ -1,2 +1,4 @@
+# This script is used to create a SSL certificate that can be used to sign drivers
+
 cd /usr/src/linux-headers-$(uname -r)/certs
 sudo openssl req -new -x509 -newkey rsa:2048 -keyout signing_key.pem -outform DER -out signing_key.x509 -nodes -subj "/CN=Owner/"
